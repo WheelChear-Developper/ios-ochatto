816cfea470b43bedcdf6d0e5d7c7a87f40172a19
Mobile Innovation, LLC <amatani@mobile-innovation.jp>
2015-06-18 11:56:38 +0900

    Timeline追加

--- ochatto/TimeLineViewController.swift
+++ ochatto/TimeLineViewController.swift
@@ -30,18 +30,28 @@ class TimeLineViewController: UIViewController, UITableViewDataSource, UITableVi
         Social_TableWakuView.hidden = false
     }
     
+    @IBAction func NewDiary_private_push(sender: AnyObject) {
+        
+        self.tabBarController?.selectedIndex = 6
+    }
+    
+    @IBAction func NewDiary_social_push(sender: AnyObject) {
+        
+        self.tabBarController?.selectedIndex = 6
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationItem.title = "MyTimeline - Private"
         
-//        Private_TableView.delegate = self
-//        Private_TableView.dataSource = self
-//        Social_TableView.delegate = self
-//        Social_TableView.dataSource = self
+        Private_TableView.delegate = self
+        Private_TableView.dataSource = self
+        Social_TableView.delegate = self
+        Social_TableView.dataSource = self
         
-//        Private_TableWakuView.hidden = false
-//        Social_TableWakuView.hidden = true
+        Private_TableWakuView.hidden = false
+        Social_TableWakuView.hidden = true
         
     }
     
@@ -64,22 +74,54 @@ class TimeLineViewController: UIViewController, UITableViewDataSource, UITableVi
     */
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if (tableView == Private_TableView){
-            return 0
+            return 4
         }else if (tableView == Social_TableView){
-            return 0
+            return 5
         }else{
             return 0
         }
     }
     
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        
+        var ret :CGFloat = 0
         if (tableView == Private_TableView){
-            return 70
+            
+            switch indexPath.row {
+            case 0:
+                ret = 120
+            case 1:
+                ret = 120
+            case 2:
+                ret = 250
+            case 3:
+                ret = 40
+            default:
+                ret = 0
+                break
+            }
+            
         }else if (tableView == Social_TableView){
-            return 70
+            
+            switch indexPath.row {
+            case 0:
+                ret = 120
+            case 1:
+                ret = 120
+            case 2:
+                ret = 120
+            case 3:
+                ret = 120
+            case 4:
+                ret = 40
+            default:
+                ret = 0
+                break
+            }
         }else{
             return 0
         }
+        return ret
     }
     
     /*
@@ -87,37 +129,45 @@ class TimeLineViewController: UIViewController, UITableViewDataSource, UITableVi
     (実装必須)
     */
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("damy_cell", forIndexPath: indexPath) as! UITableViewCell
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell-damy", forIndexPath: indexPath) as! UITableViewCell
         if (tableView == Private_TableView){
             if(indexPath.row == 0){
-                let cell = tableView.dequeueReusableCellWithIdentifier("tab1_cell1", forIndexPath: indexPath) as! UITableViewCell
+                let cell = tableView.dequeueReusableCellWithIdentifier("cell1", forIndexPath: indexPath) as! UITableViewCell
             }
             
             if(indexPath.row == 1){
-                let cell = tableView.dequeueReusableCellWithIdentifier("tab1_cell2", forIndexPath: indexPath) as! UITableViewCell
+                let cell = tableView.dequeueReusableCellWithIdentifier("cell2", forIndexPath: indexPath) as! UITableViewCell
             }
             
             if(indexPath.row == 2){
-                let cell = tableView.dequeueReusableCellWithIdentifier("tab1_cell3", forIndexPath: indexPath) as! UITableViewCell
+                let cell = tableView.dequeueReusableCellWithIdentifier("cell3", forIndexPath: indexPath) as! UITableViewCell
+            }
+            
+            if(indexPath.row == 3){
+                let cell = tableView.dequeueReusableCellWithIdentifier("cell-new", forIndexPath: indexPath) as! UITableViewCell
             }
             
         }else
             
             if (tableView == Social_TableView){
                 if(indexPath.row == 0){
-                    let cell = tableView.dequeueReusableCellWithIdentifier("tab2_cell1", forIndexPath: indexPath) as! UITableViewCell
+                    let cell = tableView.dequeueReusableCellWithIdentifier("cell1", forIndexPath: indexPath) as! UITableViewCell
                 }
                 
                 if(indexPath.row == 1){
-                    let cell = tableView.dequeueReusableCellWithIdentifier("tab2_cell2", forIndexPath: indexPath) as! UITableViewCell
+                    let cell = tableView.dequeueReusableCellWithIdentifier("cell2", forIndexPath: indexPath) as! UITableViewCell
                 }
                 
                 if(indexPath.row == 2){
-                    let cell = tableView.dequeueReusableCellWithIdentifier("tab2_cell3", forIndexPath: indexPath) as! UITableViewCell
+                    let cell = tableView.dequeueReusableCellWithIdentifier("cell3", forIndexPath: indexPath) as! UITableViewCell
                 }
                 
                 if(indexPath.row == 3){
-                    let cell = tableView.dequeueReusableCellWithIdentifier("tab2_cell3", forIndexPath: indexPath) as! UITableViewCell
+                    let cell = tableView.dequeueReusableCellWithIdentifier("cell4", forIndexPath: indexPath) as! UITableViewCell
+                }
+                
+                if(indexPath.row == 4){
+                    let cell = tableView.dequeueReusableCellWithIdentifier("cell-new", forIndexPath: indexPath) as! UITableViewCell
                 }
                 
             }else{
